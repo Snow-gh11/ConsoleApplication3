aba9479cfdc22dfcb383372e53b7cda24155081b
Amyas GHEROUS <amyas.gherous.dz@viacesi.fr>
2020-11-02 18:13:34 +0100

    Ajoutez des fichiers projet.

---
+++ ConsoleApplication2/ConsoleApplication2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <math.h>
+int main()
+{
+	int Xa ;
+	int distancex, distancey;
+	std::cout << "Entrez la valeur du x du premier point : ";
+	std::cin >> Xa=5;
+	std::cout << "Xa est de :" << (Xa);
+
+	/*std::cout << "Entrez la valeur du y du premier point : ";
+	std::cin >> Ya;
+	std::cout << "Entrez la valeur du x du deuxieme point : ";
+	std::cin >> Xb;
+	std::cout << "Entrez la valeur du y du deuxieme point : ";
+	std::cin >> Yb;
+	distancex = (Xb - Xa) ^ 2;
+	distancey = (Yb - Ya) ^ 2;
+	std::cout << "la distance de ces deux points sont de : " << sqrt(distancex + distancey); 
+
+	Xa = 0;
+	std:: cin >> Xa;*/
+	return 0;
+}
